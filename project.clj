01e69d686fcfa073cf4495054dab34f2f5b1ae07
Karsten Schmidt <k@postspectacular.com>
2013-03-31 04:09:09 +0100

    initial check in

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject resonate2013 "1.0.0"
+  :description "a collection examples created for and during the Clojure & Overtone workshop at Resonate 2013."
+  :url "http://hg.postspectacular.com/resonate-2013"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.8.1"]
+                 [incanter "1.4.1"]
+                 [quil "1.6.0"]])
